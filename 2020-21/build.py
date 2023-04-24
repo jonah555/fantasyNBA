8fa2dba6bf800bc578ddadaf4399f74892cf48f5
jonah555 <43618590+jonah555@users.noreply.github.com>
2023-04-23 21:14:43 -0700

    Add files via upload

---
+++ 2020-21/build.py
@@ -0,0 +1,49 @@
+import json
+
+database_team = json.load(open('teams_2021.json'))
+database_player = json.load(open('players_2021.json'))
+database_stats = json.load(open('stats_2021.json'))
+database_games = json.load(open('games_2021.json'))
+database_player_id = json.load(open('name_to_id.json'))
+
+
+def lookup_stats(game_id, player_id):
+
+    game_stats = database_stats[f'{game_id}_{player_id}']
+    team = game_stats['TEAM']
+    opponent = database_games[game_id]['team'][0] if database_games[game_id]['team'][1] == team else database_games[game_id]['team'][1]
+    stats = {'opponent': opponent}
+    stats.update(game_stats)
+
+    return stats
+
+
+def get_player_stats(player_id):
+
+    player_info = database_player[player_id]
+    print('JONAHHHHHH')
+    stats = {'Name': player_info['name'], 'Game Log': []}
+
+    for game in player_info['games']:
+        stats['Game Log'].append(lookup_stats(game, player_id))
+    
+    return stats
+
+
+print('Please type in the full name of the player:')
+player_name_to_search = input()
+
+player_id = str(database_player_id[player_name_to_search])
+
+player_stats = get_player_stats(player_id)
+with open(f'{player_name_to_search}_2021.json', 'w') as f:
+    json.dump(player_stats, f)
+
+
+
+
+
+
+
+
+
